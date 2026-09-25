1ebc23add8d3b774a3dfa4d1e7bdaa1162bb3064
Swetha Radhakrishnan <60323194+swetha181@users.noreply.github.com>
2026-09-26 01:26:55 +0930

    Add minime public files

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE="minime-v1";self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/favicon.svg"]))));self.addEventListener("fetch",event=>{if(event.request.method==="GET")event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
